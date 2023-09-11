ff9311670759fa8e2080d3e8e6095ca818099357
Joshua Cheng <joshuacheng888@gmail.com>
2023-09-11 14:04:11 -0700

    Initial Commit

---
+++ Bid/BidApp.swift
@@ -0,0 +1,17 @@
+//
+//  BidApp.swift
+//  Bid
+//
+//  Created by Joshua Cheng on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
